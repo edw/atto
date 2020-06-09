d1210e62122bd55727628abb24c70caa731e3770
edw <edw@poseur.com>
2020-06-09 11:54:16 +0000

    implement basic parser infrastructure

--- atto.c
+++ atto.c
@@ -2,6 +2,7 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef int (*get_char_fn)(void *);
 typedef void (*unget_char_fn)(void *, int);
@@ -11,10 +12,10 @@ enum { FALSE, TRUE };
 typedef enum token_type {
   UNKNOWN=-2, END=-1, TAG=0,
   SYMBOL=1, INTEGER=2, STRING=3,
-  OPEN_PAREN=4, CLOSE_PAREN=5
+  OPEN_PAREN=4, CLOSE_PAREN=5, APOSTROPHE=6
 } token_type;
 
-typedef struct lexer_state {
+typedef struct lexer {
   void *cookie;
   long current_line_no;
   long current_line_pos;
@@ -28,7 +29,7 @@ typedef struct lexer_state {
   char *token_chs;
   char n_token_chs;
   int token_chs_cap;
-} lexer_state;
+} lexer;
 
 typedef struct token {
   int type;
@@ -40,9 +41,9 @@ typedef struct token {
 const int FIRST_LINE_NO = 1;
 const int PRE_FIRST_LINE_POS = -1;
 
-void lexer_next_char(lexer_state *s);
+void lexer_next_char(lexer *s);
 
-void init_lexer(lexer_state *s, get_char_fn get_char, unget_char_fn unget_char) {
+void init_lexer(lexer *s, get_char_fn get_char, unget_char_fn unget_char) {
   s->get_char = get_char;
   s->unget_char = unget_char;
   s->current_char = '\0';
@@ -54,7 +55,12 @@ void init_lexer(lexer_state *s, get_char_fn get_char, unget_char_fn unget_char) 
   lexer_next_char(s);
 }
 
-void lexer_append(lexer_state *s, int new_ch) {
+void destory_lexer(lexer *s) {
+  if (s->token_chs != NULL)
+    free(s->token_chs);
+}
+
+void lexer_append(lexer *s, int new_ch) {
   char ch = (char)new_ch;
 
   if (ch == '\0')
@@ -80,7 +86,7 @@ void lexer_append(lexer_state *s, int new_ch) {
   s->token_chs[s->n_token_chs] = '\0';
 }
 
-void lexer_next_char(lexer_state *s) {
+void lexer_next_char(lexer *s) {
   int ch = s->get_char(s->cookie);
   lexer_append(s, s->current_char);
   s->current_offset++;
@@ -115,6 +121,10 @@ char* token_description_alloc(token *t) {
     break;
   case STRING:
     asprintf(&message, "STRING (%s) at %d:%d", t->chs, line, col);
+    break;
+  case APOSTROPHE:
+    asprintf(&message, "APOSTROPHE at %d:%d", line, col);
+    break;
   default:
     asprintf(&message, "Token type %d at %d:%d", t->type, line, col);
     break;
@@ -122,17 +132,17 @@ char* token_description_alloc(token *t) {
   return message;
 };
 
-int lexer_peek(lexer_state *s) {
+int lexer_peek(lexer *s) {
   int ch = s->get_char(s->cookie);
   s->unget_char(s->cookie, ch);
   return ch;
 }
 
-void lexer_abort(lexer_state *s, char *message) {
+void lexer_abort(lexer *s, char *message) {
 
 }
 
-void token_set(token* t, lexer_state *s, int token_type) {
+void token_set(token* t, lexer *s, int token_type) {
   t->chs = s->token_chs;
   t->line_no = s->token_line_no;
   t->line_pos = s->token_line_pos;
@@ -140,17 +150,18 @@ void token_set(token* t, lexer_state *s, int token_type) {
 }
 
 int isnumsym(int ch) {
-  if((ch == '(') || (ch == ')') || (ch == '"') || isspace(ch))
+  if((ch == '(') || (ch == ')') || (ch == '"') || (ch == '\'')
+     || isspace(ch))
     return FALSE;
 
   return TRUE;
 }
 
-void lexer_skip_whitespace_and_comments(lexer_state *s) {
+void lexer_skip_whitespace_and_comments(lexer *s) {
   do {
     while(isspace(s->current_char))
       lexer_next_char(s);
-    if(s->current_char == '#') {
+    if(s->current_char == ';') {
       while(s->current_char != '\n')
         lexer_next_char(s);
       lexer_next_char(s);
@@ -160,7 +171,7 @@ void lexer_skip_whitespace_and_comments(lexer_state *s) {
   } while(TRUE);
 }
 
-void lexer_reset_token(lexer_state *s) {
+void lexer_reset_token(lexer *s) {
   if (s->token_chs_cap == -1) {
     s->token_chs = calloc(1, sizeof(char));
     if (s->token_chs == NULL)
@@ -173,7 +184,7 @@ void lexer_reset_token(lexer_state *s) {
   s->token_chs[s->n_token_chs] = '\0';
 }
 
-int lexer_get_token(lexer_state *s, token *t) {
+int lexer_get_token(lexer *s, token *t) {
   token_type inferred_type;
 
   lexer_skip_whitespace_and_comments(s);
@@ -181,6 +192,10 @@ int lexer_get_token(lexer_state *s, token *t) {
 
   if (s->current_char == '\0') {
     token_set(t, s, END);
+    lexer_next_char(s);
+
+  } else if (s->current_char == '\'') {
+    token_set(t, s, APOSTROPHE);
     lexer_next_char(s);
 
   } else if (s->current_char == '(') {
@@ -236,9 +251,56 @@ int lexer_get_token(lexer_state *s, token *t) {
   return (t->type != END);
 }
 
+/* Parser */
+
+typedef struct parser {
+  lexer lex;
+  token current_token, next_token;
+} parser;
+
+void parser_next_token(parser *p);
+
+void init_parser(parser *p, get_char_fn get, unget_char_fn unget) {
+  init_lexer(&p->lex, get, unget);
+  p->current_token.chs = p->next_token.chs = NULL;
+  parser_next_token(p);
+  parser_next_token(p);
+}
+
+void destroy_parser(parser *p) {
+  destory_lexer(&p->lex);
+  if (p->current_token.chs != NULL)
+    free(p->current_token.chs);
+}
+
+void dump_parser(parser *p) {
+  fprintf(stderr, "<Parser %p current_token.chs: %p next_token.chs: %p>\n",
+          p, p->current_token.chs, p->next_token.chs);
+}
+
+void parser_next_token(parser *p) {
+  char *chs;
+
+  if (p->current_token.chs != NULL)
+    free(p->current_token.chs);
+
+  if (p->next_token.chs != NULL) {
+    chs = strdup(p->next_token.chs);
+    if (chs == NULL)
+      exit(EXIT_FAILURE);
+  } else {
+    chs = NULL;
+  }
+
+  p->current_token = p->next_token;
+  p->current_token.chs = chs;
+
+  lexer_get_token(&p->lex, &p->next_token);
+}
+
+/* Main code */
 
 int ungotten = '\0';
-
 void stdin_unget_char(void *cookie, int ch) {
   ungotten = ch;
 }
@@ -260,18 +322,20 @@ int stdin_get_char(void *cookie) {
 }
 
 int main(int argc, char **argv) {
-  lexer_state lex;
-  token token;
   char *description;
+  parser parser;
 
-  init_lexer(&lex, stdin_get_char, stdin_unget_char);
+  init_parser(&parser, stdin_get_char, stdin_unget_char);
 
-  while (lexer_get_token(&lex, &token)) {
-    description = token_description_alloc(&token);
+  while (parser.current_token.type != END) {
+    description = token_description_alloc(&parser.current_token);
     if (description == NULL)
       exit(EXIT_FAILURE);
     printf("%s\n", description);
     free(description);
+    parser_next_token(&parser);
   }
+
+  destroy_parser(&parser);
   return 0;
 }
